1fcd7d21ed7e436e860640dfa0201c393b1dfd65
YOUR FULL NAME <YOUR EMAIL ADDRESS>
2026-03-14 15:52:56 -0800

    HTTP client gives an error, request works

---
+++ app/src/main/java/matrix/calculation/App.java
@@ -0,0 +1,19 @@
+package matrix.calculation;
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+
+public class App {
+    private static final String API_URL = "https://www.thebluealliance.com/api/v3";
+    public static void main(String[] args) throws IOException{
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+        .url(API_URL).build();
+        try(Response response = client.newCall(request).execute()){
+            System.out.println(response.body().string());
+        }
+    }
+}
