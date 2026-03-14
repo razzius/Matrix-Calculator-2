af49ae245307b5507fa56a34f4cefa4a4b758be6
Razzi Abuissa <razzi@abuissa.net>
2026-03-14 16:43:45 -0500

    Add dotenv to keep api key out of git and make url /team/frc167/events

--- app/src/main/java/matrix/calculation/App.java
+++ app/src/main/java/matrix/calculation/App.java
@@ -5,14 +5,19 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 
 public class App {
     private static final String API_URL = "https://www.thebluealliance.com/api/v3";
     public static void main(String[] args) throws IOException{
+        Dotenv dotenv = Dotenv.load();
+        String apiKey = dotenv.get("API_KEY");
         OkHttpClient client = new OkHttpClient();
-        final String STATUS_URL = API_URL + "/status";
+        final String STATUS_URL = API_URL + "/team/frc167/events";
         Request request = new Request.Builder()
-        .url(STATUS_URL).build();
+          .addHeader("X-TBA-Auth-Key", apiKey)
+          .url(STATUS_URL).build();
         try(Response response = client.newCall(request).execute()){
             System.out.println(response.body().string());
         }
